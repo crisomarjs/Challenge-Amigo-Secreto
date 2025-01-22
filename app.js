c2c63ee2552c43409ad6934d59d076a6ceeb6d65
crisomarjs <cristianomar71@outlook.com>
2025-01-22 16:57:26 -0600

    Se crea funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo(){
+
+    let amigoNuevo = document.getElementById('amigo').value;
+
+    //Validacion de campos vacios
+    if(amigoNuevo === ''){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    //Listado de los amigos agregados
+    amigos.push(amigoNuevo);
+    document.getElementById('amigo').value = "";
+}
