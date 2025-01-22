8dcbe3e66c0202476fb1d6494694dd556ed25967
crisomarjs <cristianomar71@outlook.com>
2025-01-22 17:05:04 -0600

    Se agrega funcion para actualizar amigo y sortear amigo

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 let amigos = [];
 
 function agregarAmigo(){
-
     let amigoNuevo = document.getElementById('amigo').value;
 
     //Validacion de campos vacios
@@ -14,4 +13,29 @@ function agregarAmigo(){
     //Listado de los amigos agregados
     amigos.push(amigoNuevo);
     document.getElementById('amigo').value = "";
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    //Se agregan los amigos y se muestran cada uno
+    for(let i = 0; i < amigos.length; i++){
+        lista.innerHTML += `${i + 1}. ${amigos[i]}<br>`;
+    }
+}
+
+function sortearAmigo(){
+    //Verifica si la lista de amigos esta vacia
+    if(amigos.length == 0){
+        alert('La lista de amigos no puede estar vacía.');
+        return;
+    }
+
+    // Genera un índice aleatorio basado en el tamaño del arreglo
+    let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+    // Muestra el nombre del amigo seleccionado aleatoriamente
+    let ganador = document.getElementById('resultado');
+    ganador.innerHTML = amigos[numeroAleatorio];
 }
